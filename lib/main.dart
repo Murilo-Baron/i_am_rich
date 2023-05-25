68e61a0d70659dfe9f466695eb71edcfaa4f467f
murilo-baron <muuhbarrow@gmai.com>
2023-05-25 10:31:21 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.attach_money,
+                size: 150,
+                color: Colors.green,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'I Am Rich',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
